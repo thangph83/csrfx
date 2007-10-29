311bfb6a952b63744048d582023630024891a832
Mario.Heiderich <Mario.Heiderich@037f2fd3-9d3d-0410-b1de-b38ad6bc2384>
2007-10-29 16:33:37 +0000

    committed latest version of the first.php with browser check, optimized get/post checking and several bugfixes

--- trunk/First.php
+++ trunk/First.php
@@ -17,14 +17,15 @@
  * @package    CSRFx
  */
 
-// needed sql:  CREATE TABLE `csrf_tokens` (`token` VARCHAR( 255 ) NOT NULL ,`session` VARCHAR( 255 ) NOT NULL ,`created` TIMESTAMP NOT NULL) ENGINE = MYISAM 
-// optional sql: ALTER TABLE `csrf_tokens` ADD INDEX ( `token` )
-// optional sql: ALTER TABLE `csrf_tokens` ADD INDEX ( `session` ) 
+// needed sql:  CREATE TABLE `csrfx_tokens` (`token` VARCHAR( 255 ) NOT NULL ,`session` VARCHAR( 255 ) NOT NULL, `agent` VARCHAR( 255 ) NOT NULL ,`created` TIMESTAMP NOT NULL) ENGINE = MYISAM 
+// optional sql: ALTER TABLE `csrfx_tokens` ADD INDEX ( `token` )
+// optional sql: ALTER TABLE `csrfx_tokens` ADD INDEX ( `session` ) 
 
 /**
- * This nclass inhabits all necessary 
- * logic for CSRFX
+ * This class inhabits all necessary 
+ * logic for CSRFx
  *
+ * @package CSRFx
  * @var private the name of the request parameter
  * @var private the token to protect forms and links - has to be null!
  * @var private the patterns for the requests to protect
@@ -33,103 +34,221 @@
  */
 class CSRFX {
     
-    private $name = '?schutz-token=';
-    private $token = null;
-    private $patterns = array('/\/admin\//i', 
-                              '/\/anlegen\//i', 
-                              '/\/loeschen\//i', 
-                              '/\/bearbeiten\//i',
-                              '/\/logout\//i');
+    private $name = 'schutz-token';
+    private $separator = '?';
+    private $token = false;
+    private $method = false;
     
-    private $application = 'cakephp'; // cakephp | wordpress | null
+    private $get_patterns = array('/\/beanstanden/i',
+                              '/\/loeschen/i', 
+                              '/\/logout/i');
+
+    private $post_patterns = array('/\/einstellungen/i', 
+                              '/\/benachrichtigungen/i',
+                              '/\/beanstanden/i',
+                              '/\/loeschen/i', 
+                              '/\/logout/i');    
+    
     private $dbh = null;
     
     /**
-     * Enter description here...
+     * The constructor makes sure the session has been started, 
+     * generates the token and starts output buffering
      *
+     * @package CSRFx
+     * @param void
+     * @return boolean true
      */
     public function __construct() {
+        
+        $this->method = strtolower($_SERVER['REQUEST_METHOD']);
         
         ob_start();
         $this->token = sha1(microtime()*rand());
         
-        if(!session_id()) {
-        	session_start();
+        if (!session_id()) {
+            session_start();
         }
 
-        if(!is_null($this->application)) {
-            require_once dirname(__FILE__) . '/applications/' . 
-                escapeshellcmd($this->application) .'.php';
-            
-            $this->dbh = new PDO(CSRFX_PATH, CSRFX_USER, CSRFX_PASS);
-        } else {
-            die('no config given');
-        }        
-        
         return true;
+    }
+
+    /**
+     * This method loads the applications profile file in which databse 
+     * connection data and patterns can be found
+     *
+     * @package CSRFx
+     * @param string the applications name/name of the profile file
+     * @throws Exception
+     * @return Object this object for pagination
+     */
+    public function loadProfile($name = false) {
+        if ($name && !preg_match('/\W/', $name)) {
+            require_once dirname(__FILE__) . '/applications/' . 
+                escapeshellcmd($name) .'.php';
+            
+            $this->dbh = new PDO(CSRFX_PATH, CSRFX_USER, CSRFX_PASS);            	
+        } else {
+            throw new Exception ('Invalid application name format');
+        }
+        
+        return $this;
     }
     
     /**
-     * Enter description here...
+     * This method checks either for request method and the request uri. 
+     * If a method besides POST and GET is requested the application will 
+     * stop with 405 headers sent.  
+     * 
+     * This method has to be called before any application 
+     * logic was evaluated.
      *
+     * @package CSRFx
+     * @param void
+     * @throws Exception
      */
     public function beginProtection() {
 
-        if($_SERVER['REQUEST_METHOD'] == 'GET' || $_SERVER['REQUEST_METHOD'] == 'POST') {
-            foreach($this->patterns as $pattern) {
-                if(preg_match($pattern, $_SERVER['REQUEST_URI'])) {
-                	if(preg_match('/'.str_replace('?', '\?', $this->name).'(\w{40})/', $_SERVER['REQUEST_URI'], $matches)) {
-	                	$statement = $this->dbh->prepare("SELECT * FROM csrf_tokens WHERE token = ?");
-	                    $statement->execute(array($matches[1]));
-	                    $result = $statement->fetch();
-						if(!$result || $result['session'] != session_id()) {
-	                        die('Possible CSRF Attack detected');		                	
-						} 
-                	}
+        #disable support for XHR //TODO: think about a solution
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) 
+            && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+            return false;
+        }  
+        
+        if($this->method == 'get' || $this->method == 'post') {
+            foreach ($this->{$this->method . '_patterns'} as $pattern) {
+                if(preg_match($pattern, $_SERVER['REQUEST_URI']) || isset($_POST[$this->name])) {
+                    #check get requests
+                    if (preg_match('/=(\w{4000})$/', $_SERVER['REQUEST_URI'], $matches)) {
+                    	$result = $this->fetchToken($matches[1]);
+                        if (!$result || $result['session'] != session_id() || $result['agent'] != md5($_SERVER['HTTP_USER_AGENT'])) {
+                            $this->evokePenalty();		                	
+                        } 
+                    }
+                    #check post requests
+                    elseif (isset($_POST[$this->name])) {
+                    	$result = $this->fetchToken($_POST[$this->name]);
+                        if(!$result || $result['session'] != session_id() || $result['agent'] != md5($_SERVER['HTTP_USER_AGENT'])) {
+                            $this->evokePenalty();                          
+                        }                         
+                    } 
+                    #no token found - penalty time!
+                    else {
+                        $this->evokePenalty();
+                    }
                 }
             }
-            
-            $statement = $this->dbh->prepare("DELETE FROM csrf_tokens WHERE session = ?");
-            $statement->execute(array(session_id()));               
-            
         } else {
-        	header('HTTP/1.1 405 Method Not Allowed');
-            die();
+            #method not allowed
+            header('HTTP/1.1 405 Method Not Allowed');
+            throw new Exception('HTTP/1.1 405 Method Not Allowed');
         }
         
         return true;
     }
     
     /**
-     * Enter description here...
-     *
+     * This method modified the markup of the application, ends the output 
+     * buffering and writes the next token to the database.
+     * 
+     * This method has to be called after the last application logic 
+     * was evaluated.
+     * 
+     * @package CSRFx
+     * @param void
+     * @return boolean 
      */
     public function endProtection() {
+        
+        #disable support for XHR //TODO: think about a solution 
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) 
+            && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+            return false;
+        }    	
         
         $this->output = ob_get_contents();
         ob_end_clean();
         
+        #add token to matching links 
         $matches = array();
-        preg_match_all('/(?:href="([^"]+))/i', $this->output, $matches);
-        foreach($matches[1] as $link) {
-            foreach($this->patterns as $pattern) {
-                if(preg_match($pattern, $link)){
-                    $this->output = str_ireplace($link, $link . $this->name . $this->token, $this->output);            
+        preg_match_all('/(?:href="([^"\']+)")/i', $this->output, $matches);
+        foreach(array_unique($matches[1]) as $link) {
+            foreach ($this->{$this->method . '_patterns'} as $pattern) {
+                if (preg_match($pattern, substr($link, 0, -1))){
+                    $this->output = str_ireplace($link, $link.$this->separator.$this->name.'='.$this->token.'"', $this->output);            
                 }
             }
         }
+
+        #add token to forms
+        $this->output = str_ireplace('</form>', '<input type="hidden" name="'.$this->name.'" value="'.$this->token.'" /></form>', $this->output);            
         
+        #and clear the table from before adding new token
+        $this->deleteTokens();
+        
+        #add new token to table
+        $statement = $this->dbh->prepare("INSERT 
+                                            INTO csrfx_tokens (token, session, agent, created) 
+                                            VALUES (:token, :session, :agent, NOW())
+                                         ");
+        $statement->bindParam(':token', $this->token, PDO::PARAM_STR, 40);
+        $statement->bindParam(':session', session_id(), PDO::PARAM_STR, strlen(session_id()));
+        $statement->bindParam(':agent', md5($_SERVER['HTTP_USER_AGENT']), PDO::PARAM_STR, 32);
+        $statement->execute(); 
+        
+        #finally print the output
         print $this->output;
-        
-        $statement = $this->dbh->prepare("INSERT INTO csrf_tokens (token, session, created) VALUES (:token, :session, NOW())");
-        $statement->bindParam(':token', $this->token);
-        $statement->bindParam(':session', session_id());
-        $statement->execute();        
         
         return true;
     }
-}
 
+    /**
+     * This method sets the 412 headers and redirects 
+     * to the index page of the application.
+     *
+     * @package CSRFx
+     * @param void
+     * @return void
+     */
+    private function evokePenalty($message = false) {
         
+        #clear table if penalty strikes
+        $this->deleteTokens();
+        
+        header('HTTP/1.1 412 Precondition Failed');
+        die('You are not authorized to view this site '.
+            ' without correct security token. Click '.
+            '<a href="/">here</a> to start over.');
+    }
+
+    /**
+     * This method deletes the tokens with the matching session_id
+     *
+     * @package CSRFx
+     * @param void
+     * @return boolean true
+     */
+    private function deleteTokens() {
+        $statement = $this->dbh->prepare("DELETE FROM csrfx_tokens WHERE session = ?");
+        $statement->execute(array(session_id()));
+
+        return true;
+    }
+    
+    /**
+     * This method returns the result set for the token query
+     *
+     * @package CSRFx
+     * @param string the token to look for
+     * @return mixed the result array or false
+     */
+    private function fetchToken($token = false) {
+        $statement = $this->dbh->prepare("SELECT * FROM csrfx_tokens WHERE token = ?");
+        $statement->execute(array($token));
+        return $statement->fetch();
+    }
+}    
+
+#create instance and start protection
 $csrf = new CSRFX();
-$csrf->beginProtection();
+$csrf->loadProfile('cakephp')->beginProtection();

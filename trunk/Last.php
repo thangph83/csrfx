3d0b97d18893624a509be16e040602108e47feb2
Mario.Heiderich <Mario.Heiderich@037f2fd3-9d3d-0410-b1de-b38ad6bc2384>
2007-10-29 09:23:10 +0000

    commited first alpha version of CSRFx

---
+++ trunk/Last.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CSRFx
+ * Requirements: PHP5
+ *
+ * Copyright (c) 2007 Mario Heiderich (http://php-ids.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package    CSRFx
+ */
+
+// This is it? Yep - this is it!
+$csrf->endProtection();
